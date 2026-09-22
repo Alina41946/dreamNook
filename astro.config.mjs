653b374905906c6c8fbb2351abbfd90b7eaf5d38
Alina <heemiii2024@gmail.com>
2026-09-22 17:27:13 +0800

    Add sitemap and Search Console verification

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,13 @@
 import {defineConfig} from 'astro/config';
+import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
   site: 'https://alina41946.github.io',
   base: '/dreamNook',
+
+  integrations: [
+    sitemap({
+      filter: (page) => !page.includes('/sArea/'),
+    }),
+  ],
 });
