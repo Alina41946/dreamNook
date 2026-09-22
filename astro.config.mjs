089150d793dc3e25396059062ff89e37b1045e0c
Alina <heemiii2024@gmail.com>
2026-09-22 11:05:49 +0800

    修改config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,6 @@
-// @ts-check
-import { defineConfig } from 'astro/config';
+import {defineConfig} from 'astro/config';
 
-// https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://alina41946.github.io',
+  base: '/dreamNook',
+});
